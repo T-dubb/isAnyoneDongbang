03a14a609d15499fef180539de3ae5dfb8552a47
hooniee0811 <sinclair0811@kaist.ac.kr>
2022-01-16 23:32:20 +0900

    junghun sign-up page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="backGround">
+      <div className="signUpBlock">
+        <img className="logo" src="dongnuit.png"/>
+        <div className="inputContainer">
+          <div className="inputBox">
+            <div className="inputTitle">name</div>
+            <input className="inputLine" placeholder='name'></input>
+          </div>
+          <div className="inputBox">
+            <div className="inputTitle">student-id</div>
+            <input className="inputLine" placeholder='student-id'></input>
+          </div>
+          <div className="inputBox">
+            <div className="inputTitle text">password</div>
+            <input className="inputLine text" placeholder='password'></input>
+          </div>
+        </div>
+        <div className="signUpBtn text">sign-up</div>
+      </div>
     </div>
   );
 }
