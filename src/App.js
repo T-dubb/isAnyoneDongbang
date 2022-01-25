a9213f7ad10c9efaf74a3cba07dd5da0689af938
hooniee0811 <sinclair0811@kaist.ac.kr>
2022-01-25 17:10:18 +0900

    request reflected & simple functions

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import './App.css';
+import SignUpPage from './SignUpPage.js'
 
 
 function App() {
   return (
-    <div className="backGround">
-      <div className="signUpBlock">
-        <img className="logo" src="dongnuit.png"/>
-        <div className="inputContainer">
-          <div className="inputBox">
-            <div className="inputTitle">name</div>
-            <input className="inputLine" placeholder='name'></input>
-          </div>
-          <div className="inputBox">
-            <div className="inputTitle">student-id</div>
-            <input className="inputLine" placeholder='student-id'></input>
-          </div>
-          <div className="inputBox">
-            <div className="inputTitle text">password</div>
-            <input className="inputLine text" placeholder='password'></input>
-          </div>
-        </div>
-        <div className="signUpBtn text">sign-up</div>
-      </div>
+    <div>
+      {/* TODO: routing */}
+      <SignUpPage/>
     </div>
   );
 }
