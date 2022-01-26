8efce114ecbce201c71a7493723c452d7bc5572d
wooil <wooilkim9@gmail.com>
2022-01-27 01:52:15 +0900

    implement core sign-up, sign-in features

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,8 +1,13 @@
 import { Link } from "react-router-dom";
 import { useState } from 'react'
 import '../styles/SignIn.css';
+import { firebaseApp } from '../firebase'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 
 function SignIn() {
+    firebaseApp();
+    const auth = getAuth();
+
     const [studentId, setStudentId] = useState("")
     const [password, setPassword] = useState("")
 
@@ -13,9 +18,16 @@ function SignIn() {
         setPassword(e.target.value)
     }
 
-    const signIn = () => {
-        //update database
-        //after implementing server...
+    const signIn = (id, password) => {
+        const email = id + "@kaldi.kaist.ac.kr";
+        signInWithEmailAndPassword(auth, email, password)
+            .then(() => {
+                alert("환영합니다, " + id + "님!");
+            })
+            .catch((error) => {
+                alert("오류 발생!");
+                console.log(error);
+            });
     }
     return (
         <div className="signInBackGround">
@@ -23,14 +35,14 @@ function SignIn() {
                 <img className="logo" src="dongnuit.png" />
                 <div className="signIn-inputArea">
                     <div className="signIn-inputContainer">
-                        <input className="signIn-inputLine" placeholder='학번' value={studentId} onChange={(evt) => inputStudentId(evt)}/>
+                        <input className="signIn-inputLine" placeholder='학번' value={studentId} onChange={(evt) => inputStudentId(evt)} />
                     </div>
                     <div className="signIn-inputContainer">
-                        <input className="signIn-inputLine" placeholder='비밀번호' value={password} onChange={(evt) => inputPassword(evt)}/>
+                        <input className="signIn-inputLine" placeholder='비밀번호' value={password} onChange={(evt) => inputPassword(evt)} />
                         <div className="signIn-findpwd">비밀번호를 잊어버리셨나요?</div>
                     </div>
                 </div>
-                <div className="signInBtn" onClick={(evt) => signIn()}>로그인</div>
+                <div className="signInBtn" onClick={(evt) => signIn(studentId, password)}>로그인</div>
                 <Link to="/signUp">
                     <div className="signIn-signUp">회원 가입하기</div>
                 </Link>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,12 @@
 import '../styles/SignUp.css'
 import { useState } from 'react'
+import { firebaseApp } from '../firebase'
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
 
 function SignUp() {
+    firebaseApp();
+    const auth = getAuth();
+
     const [userName, setUserName] = useState("")
     const [studentId, setStudentId] = useState("")
     const [password, setPassword] = useState("")
@@ -16,9 +21,16 @@ function SignUp() {
         setPassword(e.target.value)
     }
 
-    const signUp = () => {
-        //update database
-        //after implementing server...
+    const signUp = (name, id, password) => {
+        const email = id + "@kaldi.kaist.ac.kr";
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(() => {
+                alert("환영합니다, " + name + "님!");
+            })
+            .catch((error) => {
+                alert("오류 발생!");
+                console.log(error);
+            });
     }
 
     return (
@@ -28,22 +40,22 @@ function SignUp() {
                 <div className="inputArea">
                     <div className="inputContainer">
                         <div className="inputTitle">이름</div>
-                        <input className="inputLine" placeholder='이름' value={userName} onChange={(evt) => inputUserName(evt)}/>
+                        <input className="inputLine" placeholder='이름' value={userName} onChange={(evt) => inputUserName(evt)} />
                     </div>
                     <div className="inputContainer">
                         <div className="inputTitle">학번</div>
-                        <input className="inputLine" placeholder='학번' value={studentId} onChange={(evt) => inputStudentId(evt)}/>
+                        <input className="inputLine" placeholder='학번' value={studentId} onChange={(evt) => inputStudentId(evt)} />
                     </div>
                     <div className="inputContainer">
                         <div className="inputTitle text">비밀번호</div>
-                        <input className="inputLine text" placeholder='비밀번호' value={password} onChange={(evt) => inputPassword(evt)}/>
+                        <input className="inputLine text" placeholder='비밀번호' value={password} onChange={(evt) => inputPassword(evt)} />
                     </div>
                     <div className="inputContainer">
                         <div className="inputTitle text">친목조</div>
-                        <input className="inputLine text" placeholder='친목조' value={password} onChange={(evt) => inputPassword(evt)}/>
+                        <input className="inputLine text" placeholder='친목조' value={password} onChange={(evt) => inputPassword(evt)} />
                     </div>
                 </div>
-                <div className="signUpBtn text" onClick={(evt) => signUp()}>회원가입</div>
+                <div className="signUpBtn text" onClick={(evt) => signUp(userName, studentId, password)}>회원가입</div>
             </div>
         </div>
     )
