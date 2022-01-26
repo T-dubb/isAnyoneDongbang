272d38fd531609adccb88897c429e34429930ff9
TaewooKim <rlaxodntttt@kaist.ac.kr>
2022-01-26 22:43:28 +0900

    [Refact]SignUp Component 수정

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,24 +1,48 @@
-import '../styles/SignUp.css';
+import '../styles/SignUp.css'
+import { useState } from 'react'
 
 function SignUp() {
+    const [userName, setUserName] = useState("")
+    const [studentId, setStudentId] = useState("")
+    const [password, setPassword] = useState("")
+
+    const inputUserName = (evt) => {
+        setUserName(evt.target.value)
+    }
+    const inputStudentId = (evt) => {
+        setStudentId(evt.target.value)
+    }
+    const inputPassword = (evt) => {
+        setPassword(evt.target.value)
+    }
+
+    const signUp = () => {
+        //update database
+        //after implementing server...
+    }
+
     return (
-        <div className="loginPageBox">
-            여기는 임시 SignUp임.
-            <div className="logoContainer" >
-                <img className="logoItem" src="styles/dniLogo.png" alt="" />
-            </div>
-            <div className="loginInputContainer" >
-                <input className="loginInputItem"
-                    placeholder="학번"
-                />
-            </div>
-            <div className="loginInputContainer" >
-                <input className="loginInputItem"
-                    placeholder="비밀번호"
-                />
+        <div className="backGround">
+            <div className="signUpBlock">
+                <img className="logo" src="dongnuit.png" />
+                <div className="inputArea">
+                    <div className="inputContainer">
+                        <div className="inputTitle">name</div>
+                        <input className="inputLine" placeholder='name' value={userName} onChange={(evt) => inputUserName(evt)}/>
+                    </div>
+                    <div className="inputContainer">
+                        <div className="inputTitle">student-id</div>
+                        <input className="inputLine" placeholder='student-id' value={studentId} onChange={(evt) => inputStudentId(evt)}/>
+                    </div>
+                    <div className="inputContainer">
+                        <div className="inputTitle text">password</div>
+                        <input className="inputLine text" placeholder='password' value={password} onChange={(evt) => inputPassword(evt)}/>
+                    </div>
+                </div>
+                <div className="signUpBtn text" onClick={(evt) => signUp()}>sign-up</div>
             </div>
         </div>
-    );
+    )
 }
 
 export default SignUp;
