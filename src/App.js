eb6209bc8f7aacfc3b8527624c411615e92f9cdc
Choo_MinJeong <mjchu9378@kaist.ac.kr>
2022-01-27 23:09:10 +0900

    StudyFlexwithT

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import {
 } from "react-router-dom";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+import SignInPractice from "./components/SignInPractice";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SignIn />} />
+        <Route path="minjeong" element={<SignInPractice />} />
         <Route path="signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
---
+++ src/components/SignInPractice.js
@@ -0,0 +1,17 @@
+import '../styles/SignInPractice.css'
+function SignInPractice(){
+    return(
+        <div className='SignInPractice-container1'>
+            <div className='SignInPractice-item1'>
+                Hello1
+            </div>
+            <div className='SignInPractice-item2'>
+                Hellllo2
+            </div>
+            <div className='SignInPractice-item3'>
+                Hellllllo3
+            </div>
+        </div>
+    );
+}
+export default SignInPractice;
