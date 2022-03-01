c842792223f0d2c40f7a2d8dc54a8951ddeda53d
TaewooKim <rlaxodntttt@kaist.ac.kr>
2022-03-01 15:38:58 +0900

    apply dummyData

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,6 +1,42 @@
 import React from 'react';
 import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
 import "../styles/HomeScreen.css";
+import { map } from '@firebase/util';
+
+const dummyData = [
+  {
+    userName: "권유진",
+    occupancyTime: 385,
+  },
+  {
+    userName: "박혜진",
+    occupancyTime: 385,
+  },
+  {
+    userName: "김태우",
+    occupancyTime: 330,
+  },
+  {
+    userName: "이중훈",
+    occupancyTime: 185,
+  },
+  {
+    userName: "나은지",
+    occupancyTime: 185,
+  },
+  {
+    userName: "서정하",
+    occupancyTime: 110,
+  },
+  {
+    userName: "추민정",
+    occupancyTime: 25,
+  },
+  {
+    userName: "김우일",
+    occupancyTime: 25,
+  },
+];
 
 function HomeScreen() {
   const auth = getAuth();
@@ -25,6 +61,9 @@ function HomeScreen() {
     return (
       <div className="HomeScreenHeader">
         This is Header
+        <button onClick={logoutClick}>
+          로그아웃!
+        </button>
       </div>
     );
   }
@@ -35,13 +74,25 @@ function HomeScreen() {
       </div>
     );
   }
-  const HomeScreenContent = () => {
+  const UserBox = ({userName, occupancyTime}) => {
     return (
       <div>
-        This is Home Screen.
-        <button onClick={logoutClick}>
-          로그아웃!
-        </button>
+        <div>{userName}</div>
+        <div>{occupancyTime}</div>
+      </div>
+    );
+  }
+  const HomeScreenContent = () => {
+    return (
+      <div className="HomeScreenBodyContainer">
+        {dummyData.map(
+          ({userName, occupancyTime}) => (
+            <UserBox
+              userName={userName}
+              occupancyTime={occupancyTime}
+            />
+          )
+        )}
       </div>
     )
   }
